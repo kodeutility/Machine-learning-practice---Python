ef2aa1e93ea3875822a0c16063ca214b7441fa48
Kiran <kodeutility@gmail.com>
2018-10-25 11:02:32 +0530

    split dataset to training and test set,added feature scaling

--- Part -1 Data Preprocessing/data_preprocessing_template.py
+++ Part -1 Data Preprocessing/data_preprocessing_template.py
@@ -1,4 +1,4 @@
-#Data preprocessing 
+ #Data preprocessing 
 
 #importing libraries
 import numpy as np
@@ -25,6 +25,23 @@ imputer = imputer.fit(X[:,1:3])
 X[:,1:3] = imputer.transform(X[:,1:3])
 
 #Encoding categorical data
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:,0] = labelencoder_X.fit_transform(X[:,0])
+
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+#split dataset to training set and test set
+#set 20% as test set
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=0)
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test =sc_X.transform(X_test)

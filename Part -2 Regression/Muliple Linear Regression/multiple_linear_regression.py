23c846755e644df94a931139c78c8ddd3936e38d
Kiran <kodeutility@gmail.com>
2018-10-27 22:58:04 +0530

    Backward elimination for muliple linear regression

--- Part -2 Regression/Muliple Linear Regression/multiple_linear_regression.py
+++ Part -2 Regression/Muliple Linear Regression/multiple_linear_regression.py
@@ -30,10 +30,59 @@ X = X[:,1:]
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=0)
 
-#Fiiting  multiple linear regression to the training set
+#Fitting  multiple linear regression to the training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train,y_train)
 
 #Predicting the test set results
 y_pred = regressor.predict(X_test)
+
+#Building the optimal model using Backward elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50,1)).astype(int),values = X, axis = 1)
+X_opt = X[:,[0,1,2,3,4,5]]
+regressor_OLS = sm.OLS(endog = y,exog = X_opt).fit()
+regressor_OLS.summary()
+
+#removing features with highest P-value and fitting the model
+X_opt = X[:,[0,1,3,4,5]]
+regressor_OLS = sm.OLS(endog = y,exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:,[0,3,4,5]]
+regressor_OLS = sm.OLS(endog = y,exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:,[0,3,5]]
+regressor_OLS = sm.OLS(endog = y,exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:,[0,3]]
+regressor_OLS = sm.OLS(endog = y,exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
